c863190ff1875148eea70c4365edaae57688da48
xCvishal <vishalsingh2402@gmail.com>
2021-04-09 18:22:54 +0530

    hero page redit

--- src/components/hero-page/hero-page.js
+++ src/components/hero-page/hero-page.js
@@ -5,12 +5,12 @@ import {Tween} from 'react-gsap';
 import './hero-page.scss';
 const HeroPage = () => (
 	<div className='hero-page'>
-	<Tween from={{opacity:0}} duration={1.5} ease="ease-in">
+	<Tween from={{opacity:0}} duration={1} ease="ease-in">
 	    <div className='hero-content'>        
           <div className='content'>
-          	<Tween from={{ y: '50px',opacity:0}} stagger={.8} duration={1.5} ease="ease-in">
-	            <h1 className='primary-heading'>Hi, I am <span>Vishal Singh</span>.  A Web Developer working mostly with react.</h1>
-	            <p>Scroll Down to know more about me.</p>
+          	<Tween from={{ y: '50px',opacity:0}} stagger={.4} duration={1} ease="ease-in">
+	            <h1 className='primary-heading'>Hi, I am <span>Vishal Singh</span>.</h1>
+	            <p>A Web developer working with React. Scroll Down to know more about me.</p>
 	            <span className='hero-button'><HeroButton /></span>  
             </Tween>        
           </div>
